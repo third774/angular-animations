552322f0491989b611b61150af3fcd21b99fa8de
Kevin Kipp <kevin.kipp@gmail.com>
2017-06-07 22:35:38 -0500

    Update route animation

--- src/app/animations/route.animations.ts
+++ src/app/animations/route.animations.ts
@@ -20,10 +20,10 @@ export const routeFadeStateTrigger = trigger('routeFadeState', [
 export const routeSlideStateTrigger = trigger('routeSlideState', [
   transition(':enter', [
     style({
-      transform: 'translateY(-100%)',
+      transform: 'translateX(-100%)',
       opacity: 0
     }),
-    animate(300)
+    animate('300ms')
   ]),
   transition(':leave', [
     animate(300, style({
